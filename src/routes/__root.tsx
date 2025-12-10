f885ba24949ff2bb9606462377c7681c1e6fff0a
Altanbagana Erdene-Ochir <altanbagana@protonmail.com>
2025-12-11 04:02:48 +0800

    feat(easter egg): added console log easter egg

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -7,6 +7,29 @@ export const Route = createRootRoute({
 })
 
 function Root() {
+  console.log(`
+========================================================================
+
+ ▄ .▄ ▄▄▄·  ▄▄▄· ▄▄▄· ▄· ▄▌   ▐ ▄ ▄▄▄ .▄▄▌ ▐ ▄▌   ▄· ▄▌▄▄▄ . ▄▄▄· ▄▄▄
+██▪▐█▐█ ▀█ ▐█ ▄█▐█ ▄█▐█▪██▌  •█▌▐█▀▄.▀·██· █▌▐█  ▐█▪██▌▀▄.▀·▐█ ▀█ ▀▄ █·
+██▀▀█▄█▀▀█  ██▀· ██▀·▐█▌▐█▪  ▐█▐▐▌▐▀▀▪▄██▪▐█▐▐▌  ▐█▌▐█▪▐▀▀▪▄▄█▀▀█ ▐▀▀▄
+██▌▐▀▐█▪ ▐▌▐█▪·•▐█▪·• ▐█▀·.  ██▐█▌▐█▄▄▌▐█▌██▐█▌   ▐█▀·.▐█▄▄▌▐█▪ ▐▌▐█•█▌
+▀▀▀ · ▀  ▀ .▀   .▀     ▀ •   ▀▀ █▪ ▀▀▀  ▀▀▀▀ ▀▪    ▀ •  ▀▀▀  ▀  ▀ .▀  ▀
+
+  Your reward for being curious:
+
+  TGJoIG5lciBuIHBoZXZiaGYgYmFyIHVodT8gTmFsam5sZiB1cmVyIGxiaCB0YiB1Z2djZjov
+  L2pqai5sYmhnaG9yLnBiei9qbmdwdT9pPXFEajRqOUp0S3BE
+
+
+
+  "It is never too late to be what you might have been."
+                                                          — George Eliot
+
+                                                        Sincerely, AILab
+========================================================================
+`)
+
   return (
     <>
       <Outlet />
